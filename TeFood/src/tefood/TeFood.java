b832fed7209fc535c307c56195387e290668415f
Trang Pham <trangpqtu00120@fpt.edu.vn>
2024-11-25 09:50:28 +0700

    25.11 Trang

--- TeFood/src/tefood/TeFood.java
+++ TeFood/src/tefood/TeFood.java
@@ -41,5 +41,6 @@ public class TeFood {
         System.out.println("Trang commit 21/11");
         System.out.println("Ha Anh Commit 21/11");
         System.out.println("Ha Anh Commit 22/11");
+        System.out.println("Trang commit 25.11");
     }
 }
