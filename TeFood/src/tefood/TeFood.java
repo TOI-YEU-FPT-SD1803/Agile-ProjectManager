f796ca120747f7c85836c3c55875afbdcab0e598
khanh minh <minhnkth03032@fpt.edu.vn>
2024-11-25 10:04:17 +0700

    merge

--- TeFood/src/tefood/TeFood.java
+++ TeFood/src/tefood/TeFood.java
@@ -44,5 +44,6 @@ public class TeFood {
         System.out.println("Trang commit 25.11");
         System.out.println("Dan Commit 25.11");
         System.out.println("Khanh Minh commit 25.11");
+        System.out.println("Khanh Minh merge 25.11");
     }
 }
