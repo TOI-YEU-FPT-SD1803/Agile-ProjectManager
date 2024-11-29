22569a863f9a574579e029f1ec23c17b65209f67
Dan-sama-1 <dantqtth02882@fpt.edu.vn>
2024-11-29 10:04:30 +0700

    dan 2

--- TeFood/src/tefood/TeFood.java
+++ TeFood/src/tefood/TeFood.java
@@ -68,5 +68,6 @@ public class TeFood {
         System.out.println("Khanh Minh merge + commit 29/11");
         System.out.println("Ha Anh merge + commit 29/11");
         System.out.println("Dan merge + commit 29/11");
+        System.out.println("Dan merge + commit 29/11 2");
     }
 }
